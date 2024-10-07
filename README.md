83d695ea1b7af03e27be2c672cf61ba67adaaa6c
vidplace7 <vidplace7@gmail.com>
2024-10-07 01:45:41 -0400

    Use argparse

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 ```yaml
 uses: vidplace7/build-meshtastic@main
 with:
+  git-ref: master
   arch: nrf52840
   board: xiao_ble
   build-script-path: bin/build-nrf52.sh
@@ -13,15 +14,16 @@ with:
 
 ## Inputs
 
-| Name                  | Required | Default   | Description |
-| --------------------- | -------- | --------- | ----------- |
-| `arch`                | True     | `esp32`   |             |
-| `board`               | True     | _None_    |             |
-| `build-script-path`   | True     | _None_    |             |
-| `remove-debug-flags`  | False    | `""`      |             |
-| `ota-firmware-source` | False    | `""`      |             |
-| `ota-firmware-target` | False    | `""`      |             |
-| `include-web-ui`      | False    | `"false"` |             |
+| Name                  | Required | Default   | Description                                                     |
+| --------------------- | -------- | --------- | --------------------------------------------------------------- |
+| `git-ref`             | True     | `master`  | The git ref (tag/branch) of the meshtastic firmware to checkout |
+| `arch`                | True     | `esp32`   |                                                                 |
+| `board`               | True     | _None_    |                                                                 |
+| `build-script-path`   | True     | _None_    |                                                                 |
+| `remove-debug-flags`  | False    | `""`      |                                                                 |
+| `ota-firmware-source` | False    | `""`      |                                                                 |
+| `ota-firmware-target` | False    | `""`      |                                                                 |
+| `include-web-ui`      | False    | `"false"` |                                                                 |
 
 
 ## Outputs
--- entrypoint.py
+++ entrypoint.py
@@ -5,20 +5,45 @@ import subprocess
 import requests
 import tarfile
 import sys
+import shutil
+import argparse
 
-inputs = {
-    'arch': os.getenv('INPUT_ARCH'),
-    'board': os.getenv('INPUT_BOARD'),
-    'build-script-path': os.path.normpath(os.getenv('INPUT_BUILD-SCRIPT-PATH')),
-    'remove-debug-flags': str(os.getenv('INPUT_REMOVE-DEBUG-FLAGS', '')).split(),
-    'ota-firmware-source': os.getenv('INPUT_OTA-FIRMWARE-SOURCE', ''),
-    'ota-firmware-target': os.getenv('INPUT_OTA-FIRMWARE-TARGET', ''),
-    'include-web-ui': bool(os.getenv('INPUT_INCLUDE-WEB-UI', False))
-}
+from git import Repo
+
+# Parse arguments
+# Handle CLI and GitHub Actions inputs
+parser = argparse.ArgumentParser(description="")
+parser.add_argument('git_ref', type=str,
+                    default="master",
+                    help='The tag or branch to clone from the repository.')
+parser.add_argument('--git_dir', type=str, required=False,
+                    # Use the GITHUB_WORKSPACE environment variable when running in GitHub Actions
+                    # Otherwise, use the 'firmware' directory (local testing)
+                    default=os.getenv('GITHUB_WORKSPACE', 'firmware'),
+                    help='The directory to clone the meshtastic repository to.')
+parser.add_argument('--arch', type=str, required=True,
+                    help='The architecture to build for.')
+parser.add_argument('--board', type=str, required=True,
+                    help='The board to build for.')
+parser.add_argument('--build_script_path', type=str, required=True,
+                    help='The path to the build script.')
+parser.add_argument('--remove_debug_flags', type=list, required=False,
+                    default=str(os.getenv('INPUT_REMOVE-DEBUG-FLAGS', '')).split(),
+                    help='The debug flags to remove from the build.')
+parser.add_argument('--ota_firmware_source', type=str, required=False,
+                    default=os.getenv('INPUT_OTA-FIRMWARE-SOURCE', ''),
+                    help='The source path to download the OTA firmware.')
+parser.add_argument('--ota_firmware_target', type=str, required=False,
+                    default=os.getenv('INPUT_OTA-FIRMWARE-TARGET', ''),
+                    help='The target path to save the OTA firmware.')
+parser.add_argument('--include_web_ui', type=bool, required=False,
+                    default=bool(os.getenv('INPUT_INCLUDE-WEB-UI', False)),
+                    help='Whether to include the web UI in the build.')
+args = parser.parse_args()
 
 env = {
     'GITHUB_ACTIONS': bool(os.getenv('GITHUB_ACTIONS')),
-    'XDG_CACHE_HOME': os.path.normpath(os.getenv('XDG_CACHE_HOME'))
+    'XDG_CACHE_HOME': os.path.normpath(os.getenv('XDG_CACHE_HOME', ''))
 }
 
 def gh_latest_release(owner, repo):
@@ -51,15 +76,27 @@ def extract_tar(tar_file, extract_to, remove_src=False):
 
 # ==============
 
-# Fix cache permissions
-if os.path.exists(env['XDG_CACHE_HOME']):
-    os.system(f'chmod -R 777 {env['XDG_CACHE_HOME']}')
+# Clone the Meshtastic repo
+if env['GITHUB_ACTIONS'] == False:
+    if os.path.exists(args.git_dir):
+        shutil.rmtree(args.git_dir)
+gh_repo = "meshtastic/firmware"
+print(f"Cloning {gh_repo} {args.git_ref} to {args.git_dir}/")
+meshtastic_repo = Repo.clone_from("https://github.com/meshtastic/firmware.git", ".firmware", single_branch=True, branch=args.git_ref, recursive=True)
+shutil.copytree(".firmware", args.git_dir, dirs_exist_ok=True)
+shutil.rmtree(".firmware")
+print(f"..Cloned {gh_repo} {args.git_ref} to {args.git_dir}/")
 
-# Workaround for the "safe.directory" issue
-os.system("git config --system --add safe.directory /github/workspace")
+if env['GITHUB_ACTIONS']:
+    # Fix cache permissions
+    if os.path.exists(env['XDG_CACHE_HOME']):
+        os.system(f'chmod -R 777 {env['XDG_CACHE_HOME']}')
+
+    # Workaround for the "safe.directory" issue
+    os.system("git config --system --add safe.directory /github/workspace")
 
 # Web UI
-if inputs['include-web-ui'] == True:
+if args.include_web_ui == True:
     mt_web = gh_latest_release('meshtastic', 'web')
     for asset in mt_web['assets']:
         if asset['name'] == 'build.tar':
@@ -69,26 +106,34 @@ if inputs['include-web-ui'] == True:
             extract_tar('build.tar','data/static', remove_src=True)
 
 # Remove debug flags for release
-if len(inputs['remove-debug-flags']) > 0:
-    for flag in inputs['remove-debug-flags']:
+if len(args.remove_debug_flags) > 0:
+    for flag in args.remove_debug_flags:
         os.system(f"sed -i /DDEBUG_HEAP/d {flag}")
 
+# Apply custom changes (if any)
+if os.path.exists('.custom'):
+    shutil.copytree(".custom/firmware", args.git_dir, dirs_exist_ok=True)
+    shutil.rmtree('.custom')
+
 # Run the Build
-sys.stdout.flush()
-r_build = subprocess.run([inputs['build-script-path'], inputs['board']], check=True)
+sys.stdout.flush()  # Fix subprocess output buffering issue
+build_abspath = os.path.abspath(os.path.join(args.git_dir, args.build_script_path))
+r_build = subprocess.run(
+    [build_abspath, args.board],
+    cwd=args.git_dir, check=True)
 
 # Pull OTA firmware
-if inputs['ota-firmware-source'] != '' and inputs['ota-firmware-target'] != '':
+if args.ota_firmware_source != '' and args.ota_firmware_target != '':
     ota_fw = gh_latest_release('meshtastic', 'firmware-ota')
     for asset in ota_fw['assets']:
-        if asset['name'] == inputs['ota-firmware-source']:
+        if asset['name'] == args.ota_firmware_source:
             # Download firmware.bin
-            download_file(asset['browser_download_url'], inputs['ota-firmware-target'])
+            download_file(asset['browser_download_url'], args.ota_firmware_target)
 
 # When running in GitHub Actions
 if env['GITHUB_ACTIONS']:
     # Hack! - Add checked-out firmware/bin to the python module path
-    sys.path.append(os.path.abspath('bin'))
+    sys.path.append(os.path.join(args.git_dir, 'bin'))
     from readprops import readProps # type: ignore
     # /Hack!
 
